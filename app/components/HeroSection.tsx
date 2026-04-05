477d5512cd6eacc0e0f390222e39e0da6b61ecbb
Nishchay kumar <phoenixgamerx15@gmail.com>
2026-04-05 22:51:15 +0530

    hero

---
+++ app/components/HeroSection.tsx
@@ -0,0 +1,9 @@
+import Spline from '@splinetool/react-spline';
+
+export default function Scene() {
+    return (
+        <div style={{ width: '100%', height: '500px' }}>
+            <Spline scene="https://my.spline.design/theeternalarc-AbfbzA1ECLa4DfoF0FgogV3K-mVr/" />
+        </div>
+    );
+}
